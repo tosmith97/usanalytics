68006e163d933d51e7d480bc0a2f04faae664ece
Tyler Otha Smith <tyler@almacampus.com>
2019-02-21 23:32:21 -0800

    crime data endpoints

--- backend/app/controllers/MainController.js
+++ backend/app/controllers/MainController.js
@@ -1,11 +1,39 @@
 const dAService = require('../services/dataAnalysis');
 
 
-exports.getDataForCountries = async function (req, res) {
+exports.getRecidivismDataForCounties = async function (req, res) {
     const { counties } = req.body;
 
     let err, results;
-    [err, results] = await to(dAService.getDataForCounties(counties));
+    [err, results] = await to(dAService.getRecidivismDataForCounties(counties));
+    if (err) return ReE(res, err);
+
+    return ReS(res, {results}, 200);
+}
+
+exports.getRecidivismDataForCalifornia = async function (req, res) {
+    let err, results;
+    [err, results] = await to(dAService.getRecidivismDataForCalifornia());
+    if (err) return ReE(res, err);
+
+    return ReS(res, {results}, 200);
+}
+
+
+exports.getCrimeRateDataForCounties = async function (req, res) {
+    const { counties } = req.body;
+
+    let err, results;
+    [err, results] = await to(dAService.getCrimeRateDataForCounties(counties));
+    if (err) return ReE(res, err);
+
+    return ReS(res, {results}, 200);
+}
+
+exports.getCrimeRateDataForCalifornia = async function (req, res) {
+
+    let err, results;
+    [err, results] = await to(dAService.getCrimeRateDataForCalifornia());
     if (err) return ReE(res, err);
 
     return ReS(res, {results}, 200);
--- backend/app/routes/v1.js
+++ backend/app/routes/v1.js
@@ -8,6 +8,26 @@ router.get('/', function(req, res, next) {
   res.json({status:"success", message:"Parcel Pending API", data:{"version_number":"v1.0.0"}})
 });
 
-router.post('/county/recidivism',           MainController.getDataForCountries);
+
+/*
+  POST Request
+  localhost:8000/v1/county/recidivism
+  Sample request:
+    {
+	    "counties": ["Alameda", "Yolo"]
+    }
+*/ 
+router.post('/county/recidivism',           MainController.getRecidivismDataForCounties);
+
+/*
+    GET Request
+    localhost:8000/v1/california/recidivism
+*/
+router.get('/california/recidivism',           MainController.getRecidivismDataForCalifornia);
+
+
+router.post('/county/crime',           MainController.getCrimeRateDataForCounties);
+router.get('/california/crime',           MainController.getCrimeRateDataForCalifornia);
+
 
 module.exports = router;
